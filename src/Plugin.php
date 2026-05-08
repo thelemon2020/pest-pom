0131d0c330c878fb2d6cdde146481cafea1b500b
thelemon2020 <thelemon.chris@gmail.com>
2026-05-08 13:29:04 -0400

    fix double registering bug

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,15 +5,8 @@ declare(strict_types=1);
 namespace Thelemon2020\PestPom;
 
 use Pest\Contracts\Plugins\Bootable;
-use Pest\TestSuite;
-use Thelemon2020\PestPom\Filters\PageObjectTestFilter;
 
 final class Plugin implements Bootable
 {
-    public function boot(): void
-    {
-        TestSuite::getInstance()
-            ->tests
-            ->addTestCaseMethodFilter(new PageObjectTestFilter);
-    }
+    public function boot(): void {}
 }
